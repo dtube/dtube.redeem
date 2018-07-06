a4f8a6efa25c5a222465695544834e6cc41b9840
wehmoen <github@wehmoen.top>
2018-07-06 16:18:55 +0200

    add optional config from file on live env

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,21 @@
-module.exports = {
+let fs = require('fs');
+
+let generalConfig = {
+    new_account_json_metadata: (name, avatar) => {
+        return {
+            profile: {
+                cover_image: 'http://i.imgur.com/aDFSk5S.jpg',
+                name,
+                profile_image: avatar
+            }
+        }
+    },
+    new_account_sp_delegation: 15,
+    webHost: 'http://localhost:8081',
+    admin_accounts: ["wehmoen","heimindanger","dtube"]
+}
+
+let sysConfig = {
     steemconnect: {
         app: 'steemconnect.app',
         scope: ['login'],
@@ -14,17 +31,12 @@ module.exports = {
     account: {
         name: 'wehmoen',
         wif: '5KRttM3wzropUwdGahNJVWC1Pd7sjh26zR5Jb3BVhWLAnbJGkwY'
-    },
-    new_account_json_metadata: (name, avatar) => {
-        return {
-            profile: {
-                cover_image: 'http://i.imgur.com/aDFSk5S.jpg',
-                name,
-                profile_image: avatar
-            }
-        }
-    },
-    new_account_sp_delegation: 15,
-    webHost: 'http://localhost:8081',
-    admin_accounts: ["wehmoen","heimindanger","dtube"]
+    }
 };
+
+if (process.env.env !== "dev") {
+    sysConfig = JSON.parse(fs.readFileSync("/run/secrets/redeemconfig.json"));
+}
+
+
+module.exports = Object.assign(generalConfig, sysConfig);
